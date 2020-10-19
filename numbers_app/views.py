a2df3e0097993aad84a2ead72dbac250386f1404
Andres  Niño <30846266+nelsnio@users.noreply.github.com>
2020-10-19 08:50:29 -0500

    remove redundant iteration

--- numbers_app/views.py
+++ numbers_app/views.py
@@ -18,11 +18,8 @@ def list(request):
 def render_list(request):
     serializer = SerializerInput(data=request.data)
     if serializer.is_valid(raise_exception=True):
-        result = []
-        for item in serializer.data['items']:
-            result.append(item) if type(item) == int else result.extend(process_data(item))
-
-        newNumberSorted=NumberSort(numbers={'items':serializer.data['items'],'result': result})
+        result = process_data(serializer.data['items'])
+        newNumberSorted = NumberSort(numbers={'items':serializer.data['items'],'result': result})
         newNumberSorted.save()
         return Response(data={'result': result}, status=status.HTTP_200_OK)
 
